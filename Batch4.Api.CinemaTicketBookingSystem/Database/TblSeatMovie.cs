103842ad1f4b468799e87b8c8d73df658a34934e
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-29 01:16:08 +0630

    [Complete] Database Table Setup

---
+++ Batch4.Api.CinemaTicketBookingSystem/Database/TblSeatMovie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Batch4.Api.CinemaTicketBookingSystem.Database;
+
+[Table("Tbl_SeatMovie")]
+public class TblSeatMovie
+{
+    [Key]
+    public int SeatMovieId { get; set; }
+    public string SeatMovieCode { get; set; }
+    public string MovieCode { get; set; }
+    public string SeatCode { get; set; }
+}
