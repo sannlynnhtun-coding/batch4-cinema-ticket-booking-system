a779ac7a7410d9578efb831eefe09fee07645285
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-29 01:15:20 +0630

    [Complete] MoularServices For Dependency Injection

---
+++ Batch4.Api.CinemaTicketBookingSystem/ModularServices.cs
@@ -0,0 +1,38 @@
+﻿using Batch4.Api.CinemaTicketBookingSystem.Database;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
+
+namespace Batch4.Api.CinemaTicketBookingSystem;
+
+public static class ModularServices
+{
+    public static IServiceCollection AddServices(this IServiceCollection services, WebApplicationBuilder builder) 
+    {
+        services.AddDataAccess();
+        services.AddBusinessLogic();
+        services.AddDbConnection(builder);
+        return services;
+    }
+
+    public static IServiceCollection AddDataAccess(this IServiceCollection services)
+    {
+        return services;
+    }
+    public static IServiceCollection AddBusinessLogic(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    public static IServiceCollection AddDbConnection(this IServiceCollection services, WebApplicationBuilder builder) 
+    {
+        string connectionString = builder.Configuration.GetConnectionString("DbConnection")!;
+        services.AddDbContext<AppDbContext>(opt =>
+        {
+            opt.UseSqlServer(connectionString);
+        }, ServiceLifetime.Transient, ServiceLifetime.Transient);
+        services.AddScoped<DbConnection>(n => new SqlConnection(connectionString));
+        return services;
+    }
+}
