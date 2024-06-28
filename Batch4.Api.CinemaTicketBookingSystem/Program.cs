9749dabe5032707fa182558e9b85ddff4d3ea6b9
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-29 01:18:18 +0630

    [Add] Inject Services in Program.cs

--- Batch4.Api.CinemaTicketBookingSystem/Program.cs
+++ Batch4.Api.CinemaTicketBookingSystem/Program.cs
@@ -1,3 +1,5 @@
+using Batch4.Api.CinemaTicketBookingSystem;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddServices(builder);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
