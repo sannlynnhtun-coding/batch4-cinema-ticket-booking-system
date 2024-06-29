a5aeaa91a151ca8ff3a0b55d117692a78dc839f4
MratRikzil <142309328+MratRikzil@users.noreply.github.com>
2024-06-29 22:23:19 +0630

    Business Logic, User Interface, Data Access for Movie [Complete]

---
+++ Batch4.Api.CinemaTicketBookingSystem/Features/Movie/BL_Movie.cs
@@ -0,0 +1,18 @@
+﻿using Batch4.Api.CinemaTicketBookingSystem.Models.MovieModel;
+
+namespace Batch4.Api.CinemaTicketBookingSystem.Features.Movie
+{
+    public class BL_Movie
+    {
+        private readonly DA_Movie _movie;
+        public BL_Movie()
+        {
+            _movie = new DA_Movie();
+        }
+        public List<MovieList> GetMovies()
+        {
+            var list = _movie.GetMovies();
+            return list;
+        }
+    }
+}
---
+++ Batch4.Api.CinemaTicketBookingSystem/Features/Movie/MovieController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Batch4.Api.CinemaTicketBookingSystem.Features.Movie
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly BL_Movie _bL_Movie;
+
+        public MovieController()
+        {
+            _bL_Movie = new BL_Movie();
+        }
+
+        [HttpGet]
+        public IActionResult Read()
+        {
+            var item = _bL_Movie.GetMovies();
+            return Ok(item);
+        }
+    }
+}
