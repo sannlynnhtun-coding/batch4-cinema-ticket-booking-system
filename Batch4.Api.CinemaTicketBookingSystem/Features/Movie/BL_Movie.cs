56b789730c0c85eaf2bed744a0a29ad249229e2c
MratRikzil <142309328+MratRikzil@users.noreply.github.com>
2024-06-30 21:01:51 +0630

    User Interface, Business Logic, Data Access (Movie Using Dapper) [Complete]

--- Batch4.Api.CinemaTicketBookingSystem/Features/Movie/BL_Movie.cs
+++ Batch4.Api.CinemaTicketBookingSystem/Features/Movie/BL_Movie.cs
@@ -5,17 +5,18 @@ namespace Batch4.Api.CinemaTicketBookingSystem.Features.Movie
 {
     public class BL_Movie
     {
-        private readonly DA_Movie _da_movie;
+        private readonly DA_Movie _dA_movie;
 
-        public BL_Movie(DA_Movie da_movie)
+        public BL_Movie(DA_Movie dA_movie)
         {
-            _da_movie = da_movie;
+            _dA_movie = dA_movie;
         }
 
-        public List<TblMovie> GetMovies()
+        public async Task<MovieList> GetMovies()
         {
-            var list = _da_movie.GetMovies();
-            return list;
+            var movie = await _dA_movie.GetMovies();
+            if (movie is null) throw new Exception("Operation Fail");
+            return movie;
         }
     }
 }
--- Batch4.Api.CinemaTicketBookingSystem/Features/Movie/MovieController.cs
+++ Batch4.Api.CinemaTicketBookingSystem/Features/Movie/MovieController.cs
@@ -15,10 +15,17 @@ namespace Batch4.Api.CinemaTicketBookingSystem.Features.Movie
         }
 
         [HttpGet]
-        public IActionResult Read()
+        public async Task<IActionResult> Read()
         {
-            var item = _bL_Movie.GetMovies();
-            return Ok(item);
+            try
+            {
+                var item = await _bL_Movie.GetMovies();
+                return Ok(item);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
         }
     }
 }
