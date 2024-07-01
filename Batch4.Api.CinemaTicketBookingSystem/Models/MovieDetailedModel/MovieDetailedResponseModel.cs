33bde1489e9532beff27e558a50b28dec17624b9
Moe Yan Htun <moeyan227970@gmail.com>
2024-07-01 13:53:26 +0630

    [Fix] Movie Detail Model

--- Batch4.Api.CinemaTicketBookingSystem/Models/MovieDetailedModel/MovieDetailedResponseModel.cs
+++ Batch4.Api.CinemaTicketBookingSystem/Models/MovieDetailedModel/MovieDetailedResponseModel.cs
@@ -1,33 +1,25 @@
-﻿namespace Batch4.Api.CinemaTicketBookingSystem.Models.MovieDetailedModel
+﻿namespace Batch4.Api.CinemaTicketBookingSystem.Models.MovieDetailedModel;
+
+public class MovieDetailResponseModel
 {
-    public class MovieDetailResponseModel
-    {
-        public MovieDetailModel Movie { get; set; }
-        public SeatList SeatNumber { get; set; }
-        public AvailableSeatList AvailableSeats { get; set; }
-    }
+    public MovieDetailModel Movie { get; set; }
+    public List<Seats> SeatNumber { get; set; }
+    public List<AvailableSeat> AvailableSeats { get; set; }
+}
 
-    public class MovieDetailModel
-    {
-        public string MovieName { get; set; }
-        public string Description { get; set; }
-        public DateTime ShowTime { get; set; }
-    }
+public class MovieDetailModel
+{
+    public string MovieName { get; set; }
+    public string Description { get; set; }
+    public DateTime ShowTime { get; set; }
+}
 
-    public class Seats
-    {
-        public string SeatCode { get; set; }
-    }
+public class Seats
+{
+    public string SeatCode { get; set; }
+}
 
-    public class SeatList
-    {
-        public List<Seats> SeatNumber { get; set; }
-    }
-
-    public class AvailableSeatList
-    {
-        public List<Seats> AvailableSeatNumber { get; set; }
-    }
-
-
+public class AvailableSeat
+{
+    public string SeatCode { get; set; }
 }
