9736ccfef03f4fa4e29a03e08557dbf7bc550149
Moe Yan Htun <moeyan227970@gmail.com>
2024-07-01 19:23:01 +0630

    [Add] Booking Search Request & Response Model

---
+++ Batch4.Api.CinemaTicketBookingSystem/Models/BookingModel/BookingSearchRequestModel.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Batch4.Api.CinemaTicketBookingSystem.Models.BookingModel;
+
+public class BookingSearchRequestModel
+{
+    public string CustomerName { get; set; }
+}
+
---
+++ Batch4.Api.CinemaTicketBookingSystem/Models/BookingModel/BookingSearchResponseModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Batch4.Api.CinemaTicketBookingSystem.Models.BookingModel;
+
+public class BookingSearchResponseModel
+{
+    public string CustomerName { get; set; }
+    public string MovieName { get; set; }
+    public DateTime ShowTime { get; set; }
+    public string SeatNumber { get; set; }
+    public DateTime BookingHistory { get; set; }
+}
